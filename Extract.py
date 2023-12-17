31ead0f71234a7cc44928a76468c13fb0e58cee5
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-12-17 01:22:53 -0600

    edit files

--- Extract.py
+++ Extract.py
@@ -3,6 +3,9 @@ from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import requests
+from datetime import datetime
+import datetime
 
 load_dotenv()
 
@@ -23,9 +26,21 @@ def get_recently_played_track():
     if sp_oauth.is_token_expired(token_info):
         token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
 
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    token_info = token_info["access_token"]
 
-    data = sp.current_user_recently_played(limit=50)
+    input_variables = {
+        "Accept" : "application/json",
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer {token}".format(token=token_info)
+    }
+     
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+
+    user_recently_played = requests.get("https://api.spotify.com/v1/me/player/recently-played?limit=50&after={time}".format(time=yesterday_unix_timestamp), headers = input_variables)
+
+    data = user_recently_played.json()
 
     song_names, artists, played_at, timestamp = [], [], [], []
 
@@ -43,4 +58,5 @@ def get_recently_played_track():
     }
 
     song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist", "played_at", "timestamp"])
+
     return song_df
